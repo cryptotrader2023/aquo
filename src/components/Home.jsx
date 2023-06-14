a7347d6441bac59f22cc1e963f91be01ce91063b
cryptotrader2023xx@proton.me <cryptotrader2023xx@proton.me>
2023-06-14 07:19:48 +0000

    wp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,9 +19,17 @@ const Home = () => {
           <p className="lead">
             Join Aquo today and unlock a world of asset liquidity possibilities. Experience seamless digital investment, trustworthy vetting processes, and a community of like-minded investors. Together, let's reshape the future of investments and achieve your financial goals.
           </p>
+	  <div className="text-center">
+            <p className="lead">Read more from our white papers</p>
+            <div className="btn-group">
+              <a href="aquosummary.pdf" className="btn btn-primary btn-lg">Aquo Summary White Paper</a>
+	     <span style={{ width: "10px" }}></span>
+              <a href="aquowp.pdf" className="btn btn-primary btn-lg ml-2">Aquo Full White Paper</a>
+            </div>
+          </div>
         </div>
       </div>
-	   <div className="text-center">
+      <div className="text-center">
         <img src="/home.png" alt="Vision Image" className="img-fluid" style={{ height: "400px" }} />
       </div>
     </div>
