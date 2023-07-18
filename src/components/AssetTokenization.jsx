5416f9af5a9186af6a04e65509e3668780b51461
cryptotrader2023xx@proton.me <cryptotrader2023xx@proton.me>
2023-07-18 14:09:07 +0000

    tokens

--- src/components/AssetTokenization.jsx
+++ src/components/AssetTokenization.jsx
@@ -40,15 +40,12 @@ const AssetTokenization = () => {
       </div>
 
 
-  <div className="text-center">
-        <img src="/token.png" alt="Token Image" className="img-fluid" style={{ height: "400px" }} />
-      </div>
 
       <div className="row">
         <div className="col">
           <h2 className="display-6">ERC20</h2>
           <p className="lead">
-            The ERC20 token powers our platform and serves as the backbone of our digital ecosystem. It allows for seamless transactions, fee payments, and incentivization for users. The ERC20 token, the Aquo token, can be used to pay for fees (similar to how BNB works), and be paid to users for referrals and usage of the platform.
+	  The Aquo token (ERC20) will be launched to power the actual Aquo platform. In a similar manner to how the BNB coin works, the Aquo token will allow users to pay fees, earn rewards, and to trade the token normally as is done for the BNB coins.
           </p>
         </div>
       </div>
@@ -57,18 +54,23 @@ const AssetTokenization = () => {
         <div className="col">
           <h2 className="display-6">ERC721</h2>
           <p className="lead">
-            ERC721 tokens represent security tokens for fractional ownership. These tokens enable users to invest in real-world assets, such as real estate or artwork, and benefit from the advantages of fractional ownership.
-	  The ERC721 tokens are NFTs are relate to the actual share ownership in the SPV (company owning the asset). The NFT has value when the investment is confirmed and if disputed an ODR process decides on the outcome.
+	    The NFTs are the actual digital representation of the SPV shares. They are minted against the actual shares in the SPV. The NFT price is fixed by an Oracle and a synthetic token which tracks the SPV financial performance (e.g. profits, dividends, retained earnings, net worth). 
           </p>
         </div>
       </div>
 
       <div className="row">
         <div className="col">
-          <h2 className="display-6">Portfolio SPV</h2>
+          <h2 className="display-6">Trading Synthetic Tokens</h2>
           <p className="lead">
-            Our platform utilizes Portfolio Special Purpose Vehicles (SPVs) to create investment opportunities and diversify risk. Through the pooling of assets, users gain access to a range of investment options and enjoy the benefits of a diversified portfolio. These are also NFTs but for a group asset and there is a vote to join the group asset.
+	    Trading NFTs and changing actual shares in the SPV would be a costly and time consuming series of events. Therefore, we will mint synthetic tokens for the NFTs on a one-to-one basis. Then as the owners trade, they can trade the synthetic tokens. This will result in token holders having claims to the NFTs. The NFT then has claims to the actual SPV shares.
           </p>
+	  <p className="lead">
+             An example follows. Alice buys 100 NFTs for an SPV which has 100 shares. Each share costs 1,000 dollars. Alice sells all the NFTs to Bob. Bob then wants to claim to the SPV shares and hence the SPV has to change ownership to Bob. Off-chain systems are maintained for security. This is a costly way to maintain shares.
+	  </p>
+	  <p className="lead">
+             It is more cost effective to trade a synthetic token and not the NFT. Hence the NFT itself is minted and claims against the shares and the NFT is owned by the SPV itself. For 100 shares being issued, the shares are all owned by the SPV and NFTs by the SPV. Then the synthetic tokens are minted and traded. Alice has 100 synthetic tokens which she sells to Bob. Then Bob has the claims against the NFTs and hence the SPV.
+	  </p>
         </div>
       </div>
 
@@ -76,7 +78,8 @@ const AssetTokenization = () => {
         <div className="col">
           <h2 className="display-6">Stablecoin Pegging</h2>
           <p className="lead">
-            Stablecoin pegging ensures price stability by linking the value of a token to a specific asset or currency. This mechanism provides confidence and predictability in asset trading, making it easier to transact and invest with reduced volatility. Therefore the value of NFT can be guaranteed for a particular asset. 
+            Market volatility for real assets will cause investors to lose money. Hence trades are fixed to an agreed price via a synthetic token which tracks price.
+
           </p>
         </div>
       </div>
