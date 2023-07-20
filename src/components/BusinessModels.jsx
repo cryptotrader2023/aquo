251c09be0b94b8b4b2ae43a8e9de7a575abe9d11
cryptotrader2023xx@proton.me <cryptotrader2023xx@proton.me>
2023-07-20 05:35:47 +0000

    pages

--- src/components/BusinessModels.jsx
+++ src/components/BusinessModels.jsx
@@ -29,12 +29,16 @@ const BusinessModels = () => {
       title: 'Tokenization and Derivatives',
     },
     {
-      id: '6akQEV3U9kI', // Video 4
-      title: 'Synthetic Tokens',
+      id: 'TK8T9aBrrCI', // Video 4
+      title: 'Vision',
     },
     {
-      id: '8QC0EOLEXyo', // Video 5
-      title: 'Securitization',
+      id: 'jJA3AR-tmwE', // Video 5
+      title: 'Security',
+    },
+    {
+      id: 'GGNmCBBhFRQ', // Video 5
+      title: 'About Us',
     },
   ];
 
@@ -51,29 +55,38 @@ const BusinessModels = () => {
   return (
     <div className="container">
       <h1 className="text-center">Business Models</h1>
+	  <div className="row">
+        <div className="col-md-12">
+	     <div className="text-center">
+        <img src="/business.png" alt="Business Image" className="img-fluid" />
+      </div>
+      </div>
+      </div>
+
       <div className="row">
         <div className="col-md-12">
-          <h2>Unlocking Innovative Business Models</h2>
+          <h2>Listing Fees</h2>
           <p className="lead">
-            At Aquo, we empower you with the creation of novel business models that were previously unexplored. Our innovative approach to asset tokenization opens doors to new investment opportunities, such as gradual homeownership, future share options, tokenization of unique assets, and global reach. These models enable you to capitalize on emerging trends, diversify your portfolio, and achieve your investment objectives in innovative ways.
+	    As investment proposals are listed on the Aquo marketplace there is a fee. 
           </p>
-          <div className="text-center">
-            <img src="/business.png" alt="Business" className="img-fluid" style={{ height: "400px" }} />
-          </div>
+          <h2>Transaction Fees</h2>
           <p className="lead">
-            Our platform serves as a catalyst for innovation and growth. By embracing the power of asset tokenization, you can explore gradual homeownership models, allowing you to gradually acquire ownership in real estate properties. This approach offers flexibility and affordability, making homeownership more accessible and manageable.
+	    When transactions are performed, buying tokens, there is a fee.
+          </p>
+          <h2>Subscription Fee</h2>
+          <p className="lead">
+	    A subscription fee to be alerted for opportunities could apply.
+          </p>
+          <h2>Contract Fee</h2>
+          <p className="lead">
+	  A fee could apply to construct a smart contract from library functions.
+          </p>
+          <h2>Sponsorship Fee</h2>
+          <p className="lead">
+	  A fee could apply to premium listings on the platform.
           </p>
           <p className="lead">
-            Future share options provide you with the opportunity to invest in companies at a predetermined price in the future. This model allows you to participate in the growth potential of promising ventures without the immediate need for substantial capital. It opens doors to exciting investment prospects and potential high returns.
-          </p>
-          <p className="lead">
-            The tokenization of unique assets revolutionizes investment possibilities. By converting assets such as artwork, collectibles, or intellectual property into digital tokens, you can access a global market of interested investors. This democratizes the ownership and trading of unique assets, unlocking liquidity and expanding your reach.
-          </p>
-          <p className="lead">
-            With Aquo, you can seize global investment opportunities. Our platform enables you to invest in assets from around the world, providing diversification and exposure to international markets. Expand your investment horizons and capitalize on emerging trends across borders.
-          </p>
-          <p className="lead">
-            Join us at Aquo as we empower you with innovative business models. Our platform offers the tools, resources, and global reach necessary to capitalize on emerging investment opportunities. Together, let's reshape the future of investments and unlock the potential of asset tokenization.
+	    The videos below show some of the core functionalities of the platform.
           </p>
           <div className="text-center">
             <div className="row justify-content-center">
--- src/components/MarketSize.jsx
+++ src/components/MarketSize.jsx
@@ -6,28 +6,29 @@ const MarketSize = () => {
       <h1 className="text-center">Market Overview</h1>
       <div className="row">
         <div className="col-md-12">
-          <h2>Unlocking Opportunities in a Vast Market</h2>
+          <h2>Market Dynamics</h2>
           <p className="lead">
-            At Aquo, we invite you to explore the vast potential of the global market. Here's an overview of the market landscape and the opportunities that await you:
+	     The main opportunity is in financial instruments with real assets being a starting point.
           </p>
-          <h3>1. Real Estate: A $300 Trillion Market</h3>
+          <h3>1. Real Assets</h3>
           <p className="lead">
-            The real estate market is a significant component of the global economy, estimated to be worth around $300 trillion. With Aquo, you can tap into this lucrative market by investing in fractional shares of real estate properties. By participating in fractional ownership, you gain access to the potential income generated from rents and the appreciation of property values, allowing you to benefit from the opportunities within the real estate sector.
+	  The real asset market is very large, with real estate alone having a value of 300 trillion dollars. Other complex markets, such as commodities, also have very high market values.
           </p>
-          <h3>2. Derivatives: A Quadrillion-Dollar Market</h3>
+          <h3>2. Derivatives</h3>
           <p className="lead">
-            Derivatives represent a massive market with a total value in the quadrillions of dollars. These financial instruments offer diverse investment opportunities, including hedging, portfolio diversification, and amplified returns. Aquo integrates derivatives into our investment ecosystem, enabling you to leverage these powerful tools and explore a vast array of strategies within the derivatives market.
+	  The traditional finance market for derivatives is valued at hundreds of trillions, with some estimates at a quadrillion dollars.
           </p>
-          <h3>3. Global Market Reach</h3>
+          <h3>3. Globalization</h3>
           <p className="lead">
-            With Aquo, you have access to a global market of investment opportunities. Our platform enables you to invest in assets from around the world, providing you with geographical diversification and exposure to international markets. Whether it's real estate properties in booming markets, emerging technologies, or promising startups, our platform empowers you to explore and seize opportunities on a global scale.
+	  The design of Aquo is for a global reach with only a phone and an internet connection being needed.
           </p>
-          <h3>4. All Amounts Welcome</h3>
+          <h3>4. Diversity</h3>
           <p className="lead">
-            At Aquo, we believe that every investor, regardless of their investment capital, should have access to the opportunities presented by asset tokenization. We welcome investors of all sizes, from individuals starting with modest investments to institutions with substantial portfolios. Our platform caters to a wide range of investors, ensuring that everyone can participate in the transformative power of asset tokenization and unlock their potential for growth.
+	   The platform is decentralized by nature and this will lead to diverse solutions.
           </p>
           <p className="lead">
-            Join us at Aquo as we navigate the vast market landscape together. With our platform, you can tap into the trillion-dollar real estate market, explore the potential of derivatives, access global investment opportunities, and rest assured that all amounts are welcome. Let us empower you to unlock the limitless possibilities that the market has to offer.
+	    This is an opportunity in a large emerging market.
+
           </p>
         </div>
       </div>
